43b85a1b4e66cb86110f9d450f13366070c08803
Ericles Willian <ewnsilva@gmail.com>
2024-03-04 22:09:43 -0300

    build: basic react configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Portfolio } from "pages";
+import { Box } from "@mui/material";
+
+const App = () => {
+  return (
+    <Box>
+      <Portfolio />
+    </Box>
+  );
+};
+
+export default App;
