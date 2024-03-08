f33678e24ad032c7576779a39226d74bf06dd684
Ericles Willian <ewnsilva@gmail.com>
2024-03-07 21:04:06 -0300

    refactor: style adjustments

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,24 @@
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import { Home, Explore, Favorite, Settings } from "@mui/icons-material";
+import { Home, Person, Star, Work, Message } from "@mui/icons-material";
 
 export const Footer = () => (
   <BottomNavigation
-    sx={{ width: "100%", position: "fixed", bottom: 0, textAlign: "center" }}
+    sx={{
+      width: "100%",
+      position: "fixed",
+      left: "50%",
+      transform: "translateX(-50%)",
+      bottom: 10,
+      textAlign: "center",
+      maxWidth: "500px",
+      backgroundColor: "transparent",
+      borderRadius: 10,
+    }}
   >
     <BottomNavigationAction label="Home" icon={<Home />} />
-    <BottomNavigationAction label="Explore" icon={<Explore />} />
-    <BottomNavigationAction label="Favorites" icon={<Favorite />} />
-    <BottomNavigationAction label="Settings" icon={<Settings />} />
+    <BottomNavigationAction label="Person" icon={<Person />} />
+    <BottomNavigationAction label="Star" icon={<Star />} />
+    <BottomNavigationAction label="Work" icon={<Work />} />
+    <BottomNavigationAction label="Message" icon={<Message />} />
   </BottomNavigation>
 );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import {
   Toolbar,
   IconButton,
   Tooltip,
+  Typography,
 } from "@mui/material";
 import { Brightness4, DarkMode, DocumentScanner } from "@mui/icons-material";
 
@@ -19,29 +20,32 @@ export const Header = () => {
 
   return (
     <Box>
-      <AppBar position="static" sx={{ flexGrow: 1 }}>
-        <Toolbar sx={{ display: "flex", justifyContent: "end" }}>
-          <Tooltip title="Download Currículo">
-            <Button
-              color="inherit"
-              aria-label="download"
-              onClick={handleDownload}
-              endIcon={<DocumentScanner />}
-            >
-              Download Curriculo
-            </Button>
-          </Tooltip>
-          <Tooltip title="Toggle Dark Mode">
-            <IconButton
-              edge="end"
-              color="inherit"
-              aria-label="dark-mode"
-              onClick={handleDarkModeToggle}
-              sx={{ ml: 2 }}
-            >
-              <Brightness4 />
-            </IconButton>
-          </Tooltip>
+      <AppBar position="fixed" sx={{ flexGrow: 1 }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography>Ericles Willian</Typography>
+          <Box display={"flex"} justifyContent={"end"}>
+            <Tooltip title="Download Currículo">
+              <Button
+                color="inherit"
+                aria-label="download"
+                onClick={handleDownload}
+                endIcon={<DocumentScanner />}
+              >
+                Download Curriculo
+              </Button>
+            </Tooltip>
+            <Tooltip title="Toggle Dark Mode">
+              <IconButton
+                edge="end"
+                color="inherit"
+                aria-label="dark-mode"
+                onClick={handleDarkModeToggle}
+                sx={{ ml: 2 }}
+              >
+                <Brightness4 />
+              </IconButton>
+            </Tooltip>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
