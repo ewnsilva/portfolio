81e75b77b633dd0fa5fa2d2202bb2e2df97e0cd7
Ericles Willian <ewnsilva@gmail.com>
2024-03-10 19:33:47 -0300

    refactor: listing images

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography, useMediaQuery } from "@mui/material";
+import { Box, Typography, useMediaQuery, CardMedia } from "@mui/material";
+import { skills } from "utils";
 
 export const Technologies = () => {
   const matchesMd = useMediaQuery("(max-width:850px)");
@@ -10,11 +11,39 @@ export const Technologies = () => {
       <Box
         border={"5px solid black"}
         sx={{
-          width: "70%",
-          height: "300px",
           margin: "20px",
+          textAlign: "center",
+          justifyItems: "center",
         }}
-      />
+      >
+        {skills
+          .filter((i) => i.category === "Technology")
+          .map(({ image, name }) => (
+            <img
+              src={image}
+              alt={name}
+              style={{ width: "75px", padding: "5px" }}
+            />
+          ))}
+        {skills
+          .filter((i) => i.category === "Software")
+          .map(({ image, name }) => (
+            <img
+              src={image}
+              alt={name}
+              style={{ width: "75px", padding: "5px" }}
+            />
+          ))}
+        {skills
+          .filter((i) => i.category === "Management")
+          .map(({ image, name }) => (
+            <img
+              src={image}
+              alt={name}
+              style={{ width: "75px", padding: "5px" }}
+            />
+          ))}
+      </Box>
     </Box>
   );
 };
