095557b93d09925939d0e37fc55cadd1cf4ba5f2
Ericles Willian <ewnsilva@gmail.com>
2024-03-10 19:33:01 -0300

    refactor: images export config

---
+++ src/utils/skills.js
@@ -0,0 +1,129 @@
+import Axios from "../assets/images/Technologies/Axios.png";
+import Boostrap from "../assets/images/Technologies/Bootstrap.png";
+import Confluence from "../assets/images/Technologies/Confluence.png";
+import CSS from "../assets/images/Technologies/CSS.png";
+import Figma from "../assets/images/Technologies/Figma.png";
+import Git from "../assets/images/Technologies/Git.png";
+import HTML from "../assets/images/Technologies/HTML.png";
+import Insomnia from "../assets/images/Technologies/Insomnia.png";
+import JavaScript from "../assets/images/Technologies/Javascript.png";
+import Jira from "../assets/images/Technologies/Jira.png";
+import LibreOffice from "../assets/images/Technologies/LibreOffice.png";
+import MicrosoftOffice from "../assets/images/Technologies/MicrosoftOffice.png";
+import React from "../assets/images/Technologies/React.png";
+import MaterialUi from "../assets/images/Technologies/MaterialUi.png";
+import Photoshop from "../assets/images/Technologies/Photoshop.png";
+import PostgreeSQL from "../assets/images/Technologies/PostgreeSQL.png";
+import ReactRouter from "../assets/images/Technologies/ReactRouter.png";
+import Scrum from "../assets/images/Technologies/Scrum.png";
+import VsCode from "../assets/images/Technologies/VsCode.png";
+import Responsive from "../assets/images/Technologies/Responsive.png";
+import Typescript from "../assets/images/Technologies/Typescript.png";
+
+export const skills = [
+  {
+    name: "HTML",
+    category: "Technology",
+    image: HTML,
+  },
+  {
+    name: "CSS",
+    category: "Technology",
+    image: CSS,
+  },
+  {
+    name: "Design Responsivo",
+    category: "Technology",
+    image: Responsive,
+  },
+  {
+    name: "Git",
+    category: "Technology",
+    image: Git,
+  },
+  {
+    name: "JavaScript",
+    category: "Technology",
+    image: JavaScript,
+  },
+  {
+    name: "Typescript",
+    category: "Technology",
+    image: Typescript,
+  },
+  {
+    name: "Boostrap",
+    category: "Technology",
+    image: Boostrap,
+  },
+  {
+    name: "React",
+    category: "Technology",
+    image: React,
+  },
+  {
+    name: "React-Router",
+    category: "Technology",
+    image: ReactRouter,
+  },
+  {
+    name: "Axios",
+    category: "Technology",
+    image: Axios,
+  },
+  {
+    name: "Material Ui",
+    category: "Technology",
+    image: MaterialUi,
+  },
+  {
+    name: "PostgreeSQL",
+    category: "Technology",
+    image: PostgreeSQL,
+  },
+  {
+    name: "Jira",
+    category: "Management",
+    image: Jira,
+  },
+  {
+    name: "Confluence",
+    category: "Management",
+    image: Confluence,
+  },
+  {
+    name: "Scrum",
+    category: "Management",
+    image: Scrum,
+  },
+  {
+    name: "Microsoft Office",
+    category: "Management",
+    image: MicrosoftOffice,
+  },
+  {
+    name: "Libre Office",
+    category: "Management",
+    image: LibreOffice,
+  },
+  {
+    name: "Figma",
+    category: "Software",
+    image: Figma,
+  },
+  {
+    name: "Insomnia",
+    category: "Software",
+    image: Insomnia,
+  },
+  {
+    name: "Photoshop",
+    category: "Software",
+    image: Photoshop,
+  },
+  {
+    name: "VsCode",
+    category: "Software",
+    image: VsCode,
+  },
+];
