053bbc048123a188a8162de2a8c744543c23d339
Ericles Willian <ewnsilva@gmail.com>
2024-06-01 22:03:03 -0300

    test: photo test for design adjustment

---
+++ src/utils/photos.js
@@ -0,0 +1,13 @@
+import AboutPhoto from "../assets/images/Perfil/AboutPhoto.png";
+import HomePhoto from "../assets/images/Perfil/HomePhoto.png";
+
+export const photos = [
+  {
+    name: "AboutPhoto",
+    image: AboutPhoto,
+  },
+  {
+    name: "HomePhoto",
+    image: HomePhoto,
+  },
+];
