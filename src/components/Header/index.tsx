6fd5b4e46e8cb3b09db3d78a9240fdd348a9e3be
Ericles Willian <ewnsilva@gmail.com>
2024-09-28 13:20:50 -0300

    feat: change theme function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,13 +8,17 @@ import {
   Typography,
 } from "@mui/material";
 import { Brightness4, DarkMode, DocumentScanner } from "@mui/icons-material";
+import useTheme from "hooks/useTheme";
 
 export const Header = () => {
+  const { darkMode, changeTheme } = useTheme();
+  const hideHeader = () => {};
+
   const handleDownload = () => {
     // Lógica para baixar o currículo
   };
 
-  const handleDarkModeToggle = () => {
+  const switchTheme = () => {
     // Lógica para alternar entre modo claro e escuro
   };
 
@@ -22,24 +26,27 @@ export const Header = () => {
     <Box>
       <AppBar position="fixed" sx={{ flexGrow: 1 }}>
         <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography>Ericles Willian</Typography>
+          <Box display={"flex"} alignItems={"baseline"}>
+            <Typography>Ericles Willian | </Typography>
+            <Typography fontSize={12} sx={{ ml: 1 }}>
+              Desenvolvedor Front-End
+            </Typography>
+          </Box>
           <Box display={"flex"} justifyContent={"end"}>
-            <Tooltip title="Download Currículo">
-              <Button
-                color="inherit"
-                aria-label="download"
-                onClick={handleDownload}
-                endIcon={<DocumentScanner />}
-              >
-                Download Curriculo
-              </Button>
-            </Tooltip>
+            <Button
+              color="inherit"
+              aria-label="download"
+              onClick={handleDownload}
+              endIcon={<DocumentScanner />}
+            >
+              Download Curriculo
+            </Button>
             <Tooltip title="Toggle Dark Mode">
               <IconButton
                 edge="end"
                 color="inherit"
                 aria-label="dark-mode"
-                onClick={handleDarkModeToggle}
+                onClick={changeTheme}
                 sx={{ ml: 2 }}
               >
                 <Brightness4 />
