d341236453cbdb5db8bafeb4fac44ae930fb370c
Ericles Willian <ewnsilva@gmail.com>
2024-11-20 12:07:27 -0300

    refactor: settings for displaying projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,7 +1,28 @@
-import { Box, Typography, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { Box, IconButton, Typography } from "@mui/material";
+
+import { projects } from "utils";
+import { ArrowLeft, ArrowRight } from "@mui/icons-material";
 
 export const Projects = () => {
-  const matchesMd = useMediaQuery("(max-width:850px)");
+  const [preview, setPreview] = useState(3);
+  const [fade, setFade] = useState(true);
+
+  const handlePrev = () => {
+    setFade(false);
+    setTimeout(() => {
+      setPreview((prev) => (prev === 0 ? projects.length - 1 : prev - 1));
+      setFade(true);
+    }, 300);
+  };
+
+  const handleNext = () => {
+    setFade(false);
+    setTimeout(() => {
+      setPreview((prev) => (prev === projects.length - 1 ? 0 : prev + 1));
+      setFade(true);
+    }, 300);
+  };
 
   return (
     <Box mb={5} display={"flex"} flexDirection={"column"} alignItems={"center"}>
@@ -10,13 +31,67 @@ export const Projects = () => {
       </Typography>
 
       <Box
+        display={"flex"}
+        flexDirection={"row"}
         border={"5px solid black"}
-        sx={{
-          width: "70%",
-          height: "600px",
-          margin: "20px",
-        }}
-      />
+        width="90%"
+        p={2}
+        margin="20px"
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        {projects.map(({ image, name, number, description }) => {
+          return (
+            number === preview && (
+              <>
+                <IconButton disabled={preview === 3} onClick={handleNext}>
+                  <ArrowLeft />
+                </IconButton>
+
+                <Box display={"flex"} width={"80%"} alignContent={"center"}>
+                  <img
+                    key={number}
+                    src={image}
+                    alt={name}
+                    style={{
+                      height: 450,
+                      opacity: fade ? 1 : 0,
+                      transition:
+                        "opacity 0.3s ease-in-out,  filter 0.2s ease-in-out",
+                      filter: "brightness(100%)",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.currentTarget.style.filter = "brightness(50%)";
+                    }}
+                    onMouseLeave={(e) => {
+                      e.currentTarget.style.filter = "brightness(100%)";
+                    }}
+                  />
+
+                  <Box
+                    sx={{
+                      p: 2,
+                      width: "68%",
+                      position: "absolute",
+                      alignContent: "center",
+                      textAlign: "justify",
+                    }}
+                  >
+                    <Typography variant="h5" mb={2}>
+                      Descrição:
+                    </Typography>
+                    <Typography>{description}</Typography>
+                  </Box>
+                </Box>
+
+                <IconButton disabled={preview === 0} onClick={handlePrev}>
+                  <ArrowRight />
+                </IconButton>
+              </>
+            )
+          );
+        })}
+      </Box>
     </Box>
   );
 };
