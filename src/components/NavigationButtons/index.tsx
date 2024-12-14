052a627145be74b579efc5f72183218653636115
Ericles Willian <ewnsilva@gmail.com>
2024-12-14 19:15:14 -0300

    refactor: style changes

--- src/components/NavigationButtons/index.tsx
+++ src/components/NavigationButtons/index.tsx
@@ -1,80 +1,57 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, SxProps } from "@mui/material";
 import { Home, Person, Star, Work, Message } from "@mui/icons-material";
+import { useTheme } from "hooks";
 
-export const NavigationButtons = () => (
-  <Box
-    sx={{
-      width: "100%",
-      position: "fixed",
-      left: "50%",
-      transform: "translateX(-50%)",
-      bottom: 10,
-      textAlign: "center",
-      maxWidth: "500px",
-      backgroundColor: "transparent",
-      borderRadius: 10,
-      p: 0,
-    }}
-  >
-    <Button
-      href="#home"
+export const NavigationButtons = () => {
+  const { darkMode } = useTheme();
+
+  const style: SxProps = {
+    border: "1px solid",
+    borderRadius: 3,
+    borderColor: "primary.main",
+    backgroundColor: darkMode
+      ? "rgba(255, 255, 255, 0.2)"
+      : "rgba(0, 0, 0, 0.1)",
+    m: 0.5,
+    stroke: "black", // define o contorno preto
+    strokeWidth: 0.2, // ajusta a largura do contorno
+  };
+  return (
+    <Box
       sx={{
-        border: "1px solid",
-        borderRadius: 3,
-        borderColor: "primary.main",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
-        m: 0.5,
+        width: "100%",
+        position: "fixed",
+        left: "50%",
+        transform: "translateX(-50%)",
+        bottom: 10,
+        textAlign: "center",
+        maxWidth: "500px",
+        backgroundColor: "transparent",
+        borderRadius: 10,
+        p: 0,
       }}
     >
-      <Home color={"primary"} />
-    </Button>
-    <Button
-      href="#about"
-      sx={{
-        border: "1px solid",
-        borderRadius: 3,
-        borderColor: "primary.main",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
-        m: 0.5,
-      }}
-    >
-      <Person color={"primary"} />
-    </Button>
-    <Button
-      href="#tech"
-      sx={{
-        border: "1px solid",
-        borderRadius: 3,
-        borderColor: "primary.main",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
-        m: 0.5,
-      }}
-    >
-      <Star color={"primary"} />
-    </Button>
-    <Button
-      href="#projects"
-      sx={{
-        border: "1px solid",
-        borderRadius: 3,
-        borderColor: "primary.main",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
-        m: 0.5,
-      }}
-    >
-      <Work color={"primary"} />
-    </Button>
-    <Button
-      href="#contact"
-      sx={{
-        border: "1px solid",
-        borderRadius: 3,
-        borderColor: "primary.main",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
-        m: 0.5,
-      }}
-    >
-      <Message color={"primary"} />
-    </Button>
-  </Box>
-);
+      <Button href="#home" sx={style}>
+        <Home
+          color={"primary"}
+          sx={{
+            textShadow:
+              "0px 0px 1px black, 1px 1px 1px black, -1px -1px 1px black",
+          }}
+        />
+      </Button>
+      <Button href="#about" sx={style}>
+        <Person color={"primary"} />
+      </Button>
+      <Button href="#tech" sx={style}>
+        <Star color={"primary"} />
+      </Button>
+      <Button href="#projects" sx={style}>
+        <Work color={"primary"} />
+      </Button>
+      <Button href="#contact" sx={style}>
+        <Message color={"primary"} />
+      </Button>
+    </Box>
+  );
+};
