af48982f99749e8367ed51864ec5de8c5fb88b7b
Ericles Willian <ewnsilva@gmail.com>
2025-02-02 23:18:33 -0300

    raw: updating component export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 export { About } from "./About";
 export { Contact } from "./Contact";
-export { NavigationButtons } from "./NavigationButtons";
 export { Header } from "./Header";
 export { Home } from "./Home";
+export { NavigationButtons } from "./NavigationButtons";
 export { Projects } from "./Projects";
 export { Technologies } from "./Technologies";
+export { Title } from "./Title";
